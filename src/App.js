8796ec2b05f5765c777d511db09480726a84ece0
vvictorious <vvictorious30@gmail.com>
2020-12-01 13:05:23 -0700

    no valid reducerss yet, but the app is not blowing up and redux is just about set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react'
 import './App.css';
+import { Provider } from 'react-redux'
+import store from './store'
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>The start of the app</h1>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>The start of the app</h1>
+      </div>
+      </Provider>
   );
 }
 
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+
+//this is where I will be importing differents reducers and passing them into the combineReducers method below
+export default combineReducers({
+
+})
