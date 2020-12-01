7c26ee6a2c65da89a43dc9103a125d7536d6d77b
vvictorious <vvictorious30@gmail.com>
2020-12-01 14:37:37 -0700

    added thee reducer for the searchForm into the root reducer and the statee is showing up in the redux devtools

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
+import searchReducer from './searchReducer'
 
 //this is where I will be importing differents reducers and passing them into the combineReducers method below
 export default combineReducers({
-
+    searchValue: searchReducer
 })
---
+++ src/reducers/searchReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    movies: null,
+    loading: false
+}
+
+const searchReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default: 
+            return state
+    }
+}
+
+export default searchReducer
