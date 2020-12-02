8eb75d1129fbd5b186894a5867529227bfd10bb6
vvictorious <vvictorious30@gmail.com>
2020-12-01 20:40:47 -0700

    making the axios call from the component to test and make sure that it works, which it does. I am getting back the movie data

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -1,9 +1,11 @@
 import { GET_MOVIES, SET_LOADING } from './types'
 import axios from 'axios'
 
+// gets movie from the db
 export const getMovies = movie => async dispatch => {
     try {
         const res = await axios.get(`http://www.omdbapi.com/?s=star wars&apikey=${process.env.REACT_APP_MOVIE_API_KEY}`)
+        console.log(res)
         dispatch({
             type: GET_MOVIES,
             payload: res.data
--- src/components/layout/search/SearchForm.js
+++ src/components/layout/search/SearchForm.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const SearchForm = () => {
 
     const [value, setValue] = useState('')
 
-    const getMovieValue = e => {
+    const getMovieValue = async e => {
         setValue(e.target.value)
         console.log(value)
+        const res = await axios.get(`http://www.omdbapi.com/?s=${e.target.value}&apikey=${process.env.REACT_APP_MOVIE_API_KEY}`)
+        console.log(res.data.Search)
     }
 
     return (
