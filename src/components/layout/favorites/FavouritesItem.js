f24ac800e83437d095c4a015796e15f452c7b214
vvictorious <vvictorious30@gmail.com>
2020-12-15 13:49:46 -0700

    added the icon component for removing a favourite film

--- src/components/layout/favorites/FavouritesItem.js
+++ src/components/layout/favorites/FavouritesItem.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
 
+import RemoveFavourite from './RemoveFavourite'
+
 const FavouritesItem = ({movie}) => {
 
     return (
         <Fragment>
             <img src={movie.Poster} alt='movie' />
             <div className='overlay d-flex align-items-center justify-content-center'>
-                <p>remove from favs</p>
+                <RemoveFavourite />
             </div>            
         </Fragment>
     )
