9f25d73d50e1678040e76e7ca813d71489a22652
vvictorious <vvictorious30@gmail.com>
2020-12-16 16:05:40 -0700

    failed a whole lot today, but I think I am on to the right solution. I wrote a commented solution out in the MovieItem component but I am out of time to import the favourites array into that component and test it today

--- src/components/layout/movies/MovieItem.js
+++ src/components/layout/movies/MovieItem.js
@@ -2,8 +2,21 @@ import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
 import { addToFavourites } from '../../../actions/movieActions'
 import AddFavourites from '../favorites/AddFavourite'
+import moviesReducer from '../../../reducers/movieReducer';
+import FavouritesItem from '../favorites/FavouritesItem';
 
 const MovieItem = ({movie, addToFavourites}) => {
+
+    // const checkIfMovieIsAlreadyInFavourites = movie => {
+    //     FavouritesItem.forEach(fave => {
+    //         console.log(fave)
+    //         if (fave.imdbID === movie.imdbID) {
+    //             console.log('this movie is already marked as a favourite')
+    //         } else {
+    //             addToFavourites(movie)
+    //         }
+    //     })
+    // }
 
     return (
         <Fragment>
@@ -16,3 +29,6 @@ const MovieItem = ({movie, addToFavourites}) => {
 }
 
 export default connect(null, {addToFavourites})(MovieItem)
+
+// I have been thinking about this all wrong. I should bring in the favourites array and the movies array
+// write a function up above that will be called on the on Click instead
