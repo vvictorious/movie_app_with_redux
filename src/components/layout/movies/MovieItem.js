77358bb06e420d5024b942808ea0550f304150a3
vvictorious <vvictorious30@gmail.com>
2020-12-17 00:36:49 -0700

    sheesh that is a kinda complicated function, but now you can not add the same movie to the favourites array

--- src/components/layout/movies/MovieItem.js
+++ src/components/layout/movies/MovieItem.js
@@ -2,33 +2,39 @@ import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
 import { addToFavourites } from '../../../actions/movieActions'
 import AddFavourites from '../favorites/AddFavourite'
-import moviesReducer from '../../../reducers/movieReducer';
-import FavouritesItem from '../favorites/FavouritesItem';
 
-const MovieItem = ({movie, addToFavourites}) => {
+const MovieItem = ({ moviesData: {favourites}, movie, addToFavourites}) => {
 
-    // const checkIfMovieIsAlreadyInFavourites = movie => {
-    //     FavouritesItem.forEach(fave => {
-    //         console.log(fave)
-    //         if (fave.imdbID === movie.imdbID) {
-    //             console.log('this movie is already marked as a favourite')
-    //         } else {
-    //             addToFavourites(movie)
-    //         }
-    //     })
-    // }
+    const favouriteCheck = movie => {
+        if (favourites.length > 0) {
+            favourites.forEach(fave => {      
+                if (fave.imdbID === movie.imdbID) {
+                    console.log('that movie is already a favourite')
+                } else {
+                    addToFavourites(movie)
+                    console.log(fave.imdbID)
+                    console.log(movie.imdbID)
+                }
+            })
+        } else {
+            addToFavourites(movie)
+        }
+    }
 
     return (
         <Fragment>
             <img src={movie.Poster} alt='movie' />
-            <div onClick={ () => addToFavourites(movie)} className='overlay d-flex align-items-center justify-content-center'>
+            <div onClick={ () => favouriteCheck(movie)} className='overlay d-flex align-items-center justify-content-center'>
 				<AddFavourites  />
             </div>            
         </Fragment>
     )
 }
 
-export default connect(null, {addToFavourites})(MovieItem)
+const mapStateToProps = state => ({
+    moviesData: state.movieReducer
+})
+export default connect(mapStateToProps, {addToFavourites})(MovieItem)
 
 // I have been thinking about this all wrong. I should bring in the favourites array and the movies array
 // write a function up above that will be called on the on Click instead
